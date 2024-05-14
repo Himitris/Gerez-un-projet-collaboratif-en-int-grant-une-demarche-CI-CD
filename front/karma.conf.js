c95d4afe8224ff39afac70bcfce1e4b1a660f6aa
Himitris <antoine.gautier83@gmail.com>
2024-05-14 11:10:09 +0200

    try sonarqube

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/bobapp"),
       subdir: ".",
-      reporters: [{ type: "cobertura" }, { type: "text-summary" }],
+      reporters: [{ type: "html" }, { type: "text-summary" }],
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
