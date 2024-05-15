4fff0d37be31e0ec833dc52e5eb8bd02721fd05b
Himitris <antoine.gautier83@gmail.com>
2024-05-15 14:13:54 +0200

    test

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/bobapp"),
       subdir: ".",
-      reporters: [{ type: "lcov" }, { type: "text-summary" }],
+      reporters: [{ type: "html" }, { type: "lcov" }, { type: "text-summary" }],
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
